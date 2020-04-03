be28199f128725e7fcfe6437c2b7e41034f201af
Zahra Jafari <jafarizahra873@gmail.com>
2020-04-03 11:52:30 +0430

    change sidebar

--- js/custom.js
+++ js/custom.js
@@ -3,24 +3,49 @@ $(document).ready(function () {
         $('.search-icon').toggleClass('active');
         $('.search-wrapper').toggleClass('active');
     });
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        items:4,
-        rtl:true,
-        margin:25,
-        nav:true,
-        loop:true,
+    $('.owl-carousel-0').owlCarousel({
+        items:1,
+        rtl: true,
+        margin: 25,
+        nav: true,
+        loop: true,
         responsive: {
-            0:{
-                items: 1
+            o:{
+                items:1
             },
             600:{
                 items:2
             },
-            1000:{
+            800:{
+                items:3
+            },
+            1200:{
+                items:1
+            }
+        }
+    })
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items: 4,
+        rtl: true,
+        margin: 25,
+        nav: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1200:{
                 items:4
             }
         }
     });
+
 });
 
