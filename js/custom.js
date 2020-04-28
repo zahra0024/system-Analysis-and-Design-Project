d71271f22361a935d88cbe1acee4bc71cf92313a
Zahra Jafari <jafarizahra873@gmail.com>
2020-04-28 12:17:15 +0430

    Add category_1 catalog

--- js/custom.js
+++ js/custom.js
@@ -51,6 +51,22 @@ $(document).ready(function () {
     });
     $('.sale-timeout-counter').startTimer();
 
+//    ios switch
+    let switches = document.querySelectorAll('.ios-switch');
+    for (var i = 0; i <= switches.length ; i++){
+        switches[i].addEventListener('click',function (event) {
+            if (this.classList.contains('active')){
+                this.classList.remove('active');
+                this.querySelector('input[type=checkbox]').checked = false;
+            }else{
+                this.classList.add('active');
+                this.querySelector('input[type=checkbox]').checked = true;
+            }
+
+
+        })
+    }
+
 });
 
 
